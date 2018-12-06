7beacd7dc9d85912982c6fa2d0b8a39bae5c4331
Guilherme Luis <guile.hm@hotmail.com>
2018-12-05 23:47:28 -0200

    Create player model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    uid = models.CharField(max_length=100)
+    username = models.CharField(max_length=50)
+    platforms = models.CharField(max_length=100)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_changed = models.DateTimeField(auto_now=True)
