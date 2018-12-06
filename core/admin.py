280f1f8247cd292346fa29953d2a077a2469f439
Guilherme Luis <guile.hm@hotmail.com>
2018-12-05 23:47:59 -0200

    Create player admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Player
 
-# Register your models here.
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'date_added')
+    list_filter = ('date_added', 'platforms')
+    search_fields = ('uid', 'username')
