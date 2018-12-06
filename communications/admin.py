9508dabd3ac3e7c163bf4fc735f4c5308da426f8
Guilherme Luis <guile.hm@hotmail.com>
2018-12-06 00:38:48 -0200

    Create communication admin

---
+++ communications/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from communications.models import Communication
+
+
+@admin.register(Communication)
+class CommunicationAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+    list_filter = ('date_added',)
+    search_fields = ('id', 'data')
