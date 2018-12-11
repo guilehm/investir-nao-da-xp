9e32bb29f0cc336cf2a60ff8578f93f76d2a1a4d
Guilherme Luis <guile.hm@hotmail.com>
2018-12-11 19:59:29 -0200

    Add long name for platforms

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Platform(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    long_name = models.CharField(max_length=100, null=True, blank=True)
 
     date_added = models.DateTimeField(auto_now_add=True)
     date_changed = models.DateTimeField(auto_now=True)
