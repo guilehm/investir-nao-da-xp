a678d0c253b715d4a3b6647fb74b6246dd71ead7
Guilherme Luis <guile.hm@hotmail.com>
2018-12-10 23:53:55 -0200

    Update player and status admins

--- players/admin.py
+++ players/admin.py
@@ -6,12 +6,12 @@ from players.models import Player, PlayerStats
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('username', 'date_added')
-    list_filter = ('date_added', 'platforms')
+    list_filter = ('date_added', 'platforms', 'seasons')
     search_fields = ('uid', 'username')
 
 
 @admin.register(PlayerStats)
 class PlayerStatsAdmin(admin.ModelAdmin):
-    list_display = ('player', 'date_added')
+    list_display = ('id', 'player', 'date_added')
     list_filter = ('player', 'date_added')
     search_fields = ('player__username', 'data')
