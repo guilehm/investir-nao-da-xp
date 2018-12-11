ad561ae4ba1ef31518383bdefca261acdcf2ab9a
Guilherme Luis <guile.hm@hotmail.com>
2018-12-10 23:54:04 -0200

    Create core admin file

---
+++ core/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from core.models import Platform, Season
+
+
+@admin.register(Platform)
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_added')
+    list_filter = ('date_added', 'date_changed')
+    search_fields = ('name',)
+
+
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_added')
+    list_filter = ('date_added', 'date_changed')
+    search_fields = ('name',)
