4278db4dd85c984093204b455c01dbc04187be77
Guilherme Luis <guile.hm@hotmail.com>
2018-12-10 23:53:35 -0200

    Create platform and season models

---
+++ core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_changed = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Season(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_changed = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
