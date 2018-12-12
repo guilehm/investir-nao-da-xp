d89a2f1081edfd266d44d1f0460518c2d5a404d3
Guilherme Luis <guile.hm@hotmail.com>
2018-12-11 22:36:49 -0200

    Update index view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from core.models import Platform
+from communications.models import Communication
+from players.models import Player
+from django import forms
+from core.forms import SearchForm
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    players = Player.objects.all()
+    platforms = Platform.objects.all()
+    form = SearchForm()
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            platform = form.cleaned_data['platforms']
+            communication = Communication.objects.create(
+                method='profile_data',
+            )
+            success = communication.communicate(platform=platform.name, username=username)
+            if success:
+                communication.create_player_stats()
+    return render(request, 'core/index.html', {
+        'players': players,
+        'platforms': platforms,
+        'form': form,
+    })
