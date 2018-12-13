6444a4a4b327bae4535a31df2b4b9b51127777b2
Guilherme Luis <guile.hm@hotmail.com>
2018-12-12 23:43:22 -0200

    Register matches at django admin

--- players/admin.py
+++ players/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from players.models import Player, PlayerStats
+from players.models import Player, PlayerStats, Matches
 
 
 @admin.register(Player)
@@ -8,10 +8,19 @@ class PlayerAdmin(admin.ModelAdmin):
     list_display = ('username', 'date_added')
     list_filter = ('date_added', 'platforms', 'seasons')
     search_fields = ('uid', 'username')
+    readonly_fields = ('date_added', 'date_changed')
 
 
 @admin.register(PlayerStats)
 class PlayerStatsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'player', 'date_added')
+    list_filter = ('player', 'date_added')
+    search_fields = ('player__username', 'data')
+    readonly_fields = ('date_added', 'date_changed')
+
+
+@admin.register(Matches)
+class MatchesAdmin(admin.ModelAdmin):
     list_display = ('id', 'player', 'date_added')
     list_filter = ('player', 'date_added')
     search_fields = ('player__username', 'data')
