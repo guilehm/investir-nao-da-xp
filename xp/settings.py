2e0bfbc2b26de55b470c837e96de2e733020fc5b
Guilherme Luis <guile.hm@hotmail.com>
2019-01-02 17:24:22 -0200

    Remove filters and formatters

--- xp/settings.py
+++ xp/settings.py
@@ -136,27 +136,10 @@ TRN_API_KEY = os.getenv('TRN_API_KEY')
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
-            'style': '{',
-        },
-        'simple': {
-            'format': '{levelname} {message}',
-            'style': '{',
-        },
-    },
-    'filters': {
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
     'handlers': {
         'console': {
             'level': 'INFO',
-            'filters': ['require_debug_true'],
             'class': 'logging.StreamHandler',
-            'formatter': 'simple'
         },
     },
     'loggers': {
@@ -164,10 +147,9 @@ LOGGING = {
             'handlers': ['console'],
             'propagate': True,
         },
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': False,
+        '': {
+            'handlers': ['console'],
+            'level': 'INFO',
         },
     }
 }
