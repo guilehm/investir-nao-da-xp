b1f672b502f4948155659937a7a1b73b273b8a6d
Guilherme <guile.hm@hotmail.com>
2019-01-19 20:46:23 -0200

    Update communication admin

--- communications/admin.py
+++ communications/admin.py
@@ -5,6 +5,6 @@ from communications.models import Communication
 
 @admin.register(Communication)
 class CommunicationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'date_added')
+    list_display = ('id', 'method', 'error', 'date_added')
     list_filter = ('date_added', 'method')
     search_fields = ('id', 'data')
