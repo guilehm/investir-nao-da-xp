abbe9a4ed2409853e78ffff9e595c2b485ab2398
Guilherme <guile.hm@hotmail.com>
2019-01-19 17:21:34 -0200

    Create player search url

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('players/search/', views.player_search, name='player-search'),
     path('players/<str:username>/', views.player_detail, name='player-detail'),
 ]
