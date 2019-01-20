359848f90d8a5b50ba6e5e97ee3224110ce55728
Guilherme <guile.hm@hotmail.com>
2019-01-20 00:43:21 -0200

    Remove manifest

--- xp/settings.py
+++ xp/settings.py
@@ -133,7 +133,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
