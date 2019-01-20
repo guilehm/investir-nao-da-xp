6519833041d9ca45d4b52fde175161a864bbec2d
Guilherme <guile.hm@hotmail.com>
2019-01-19 22:12:46 -0200

    Increase request time and add home button at menu

--- core/tasks.py
+++ core/tasks.py
@@ -18,4 +18,4 @@ def get_friends_status():
     for friend in Friend.objects.all():
         player = friend.player
         get_profile_data(player.username, player.last_platform())
-        time.sleep(2.1)
+        time.sleep(2.5)
