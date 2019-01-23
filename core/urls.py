e6e9eab32333f7287847696829e79d1886e98ba6
Guilherme Luis <guile.hm@hotmail.com>
2019-01-22 22:28:07 -0200

    Create player detail by season url

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('players/search/', views.player_search, name='player-search'),
     path('players/<str:username>/', views.player_detail, name='player-detail'),
+    path(
+        'players/<str:username>/season/<str:season_name>/',
+        views.player_detail_by_season,
+        name='player-detail-by-season'
+    ),
 ]
