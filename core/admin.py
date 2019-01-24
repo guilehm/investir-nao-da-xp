0eb6f24189328dec815a1d6f7bfc276b9911b057
Guilherme Luis <guile.hm@hotmail.com>
2019-01-24 21:06:51 -0200

    Create item admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from core.models import Platform, Season
+from core.models import Platform, Season, Item
 
 
 @admin.register(Platform)
@@ -15,3 +15,10 @@ class SeasonAdmin(admin.ModelAdmin):
     list_display = ('name', 'date_added')
     list_filter = ('date_added', 'date_changed')
     search_fields = ('name',)
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'rarity', 'obtained')
+    list_filter = ('name', 'item_id', 'captial', 'type', 'rarity', 'obtained')
+    search_fields = ('name', 'item_id')
